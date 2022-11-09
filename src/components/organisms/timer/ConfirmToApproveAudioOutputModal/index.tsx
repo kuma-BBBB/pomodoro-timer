57c3b15fc0c46e0dafa92c4858fca52c8e421586
ofuji-works <ofuji.works@gmail.com>
2022-11-09 16:47:16 +0900

    ConfirmToApproveAudioOutputModal responsive 対応

--- src/components/organisms/timer/ConfirmToApproveAudioOutputModal/index.tsx
+++ src/components/organisms/timer/ConfirmToApproveAudioOutputModal/index.tsx
@@ -18,13 +18,14 @@ export const Presenter = ({
 }) => {
   return (
     <BackDrop open={open}>
-      <div className="bg-white p-8 w-1/3 min-w-600 max-w-800 rounded-md shadow-md">
+      <div className="bg-white p-8 md:w-1/2 md:max-w-md max-md:w-11/12 rounded-md shadow-md">
         <VStack className="gap-6 justify-center items-center">
           <span className="material-icons-outlined !text-4xl text-gray-400">
             volume_up
           </span>
           <p className="text-gray-400">
-            当サイトでは、一部機能にてオーディオが出力されます。出力を希望されない方は、Noを選択してください。
+            当サイトでは、一部機能にてオーディオが出力されます。出力を希望されない方は、
+            <strong>No</strong>を選択してください。
             <br />
             ※&nbsp;<strong>No</strong>
             を選択した場合でも、
