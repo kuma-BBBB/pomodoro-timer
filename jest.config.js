ebbe5510c7b659e8d1a86b10691fc2cbff98649f
ofuji-works <ofuji.works@gmail.com>
2022-11-15 01:57:12 +0900

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+export default {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: '.coverage',
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['**/*.spec.tsx'],
+  setupFiles: ['./jest.setup.js'],
+}
